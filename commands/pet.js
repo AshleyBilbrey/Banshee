a2e2b507970863956b903deab9b69f255de23545
Ashley Bilbrey <44065883+AshleyBilbrey@users.noreply.github.com>
2023-07-14 21:55:47 -0700

    Migrate initial versions of node and discord.js

--- commands/pet.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	name: 'pet',
-	description: 'pet',
-	execute(message, args, client) {
-		message.channel.send('What am I to you, a cow to pet?');
-        message.channel.send("https://tenor.com/view/chika-fujiwara-hit-cute-kawaii-anime-gif-13583613");
-	},
-};
--- index.js
+++ index.js
@@ -1,64 +1,28 @@
-const Discord = require('discord.js');
-const fs = require("fs");
-const bn = require("./util/bonkNum.js")
-const fc = require("./util/filterCatch.js")
-const is = require("./util/isSuper.js")
-require('dotenv').config();
+import { REST } from '@discordjs/rest';
+import { WebSocketManager } from '@discordjs/ws';
+import { GatewayDispatchEvents, GatewayIntentBits, InteractionType, MessageFlags, Client } from '@discordjs/core';
+import 'dotenv/config'
 
-const client = new Discord.Client();
-client.commands = new Discord.Collection();
+const token = process.env.BOT_TOKEN;
 
-const prefix = process.env.PREFIX;
+const rest = new REST({ version: '10' }).setToken(token);
 
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	client.commands.set(command.name, command);
-}
-
-client.on('message', message => {
-
-	if (!message.content.startsWith(prefix) || message.author.bot) {
-		let iscb = function(isSuper) {
-			if(!isSuper) {
-				let cb = function() {
-					message.delete();
-					message.channel.send("Hi **" + message.author.username + "#" + message.author.discriminator + "**! Something in your message is in our spam filter. If you beleive this is a mistake, please contact one of the " + process.env.PREFIX + " supers. Thanks!").then(reply => {
-						setTimeout(() => {
-							reply.delete();
-						}, 5000);
-					})
-	
-				}
-				fc.catch(message.content, cb);
-			}
-		}
-
-		is.isSuper(message.author, iscb)
-		
-		
-	} else {
-		const args = message.content.slice(prefix.length).trim().split(/ +/);
-		const command = args.shift().toLowerCase();
-
-		if (!client.commands.has(command)) return;
-
-		try {
-			client.commands.get(command).execute(message, args, client);
-		} catch (error) {
-			console.error(error);
-			message.reply('Sorry, there was an issue trying to run that command!');
-		}
-	}
-
-	
+const gateway = new WebSocketManager({
+    token,
+    intents: GatewayIntentBits.DirectMessages,
+    rest,
 });
 
+const client = new Client({ rest, gateway });
 
-client.on("ready", () => {
-    console.log(`Banshee logged in as ${client.user.tag}`);
-    bn.setNum(client);
+client.on(GatewayDispatchEvents.InteractionCreate, async ({ data: interaction, api }) => {
+    if (interaction.type !== InteractionType.ApplicationCommand || interaction.data.name !== 'ping') {
+        return;
+    }
+
+    await api.interactions.reply(interaction.id, interaction.token, { content: 'Pong!', flags: MessageFlags.Ephemeral });
 });
 
-client.login(process.env.BOT_TOKEN);
+client.once(GatewayDispatchEvents.Ready, () => console.log('Ready!'));
+
+gateway.connect();
